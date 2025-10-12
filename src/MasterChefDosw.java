8767e1fb8ae5e641074dea68bd225634dfa68f98
carolina.cepeda-v <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-10-11 19:27:10 -0500

    feat : estructura del proyecto

---
+++ src/MasterChefDosw.java
@@ -0,0 +1,11 @@
+package edu.dosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterChefDosw {
+    public static void main(String[] args) {
+        SpringApplication.run(MasterChefDosw.class, args);
+    }
+}
